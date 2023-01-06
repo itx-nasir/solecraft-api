7b877840c2e9df0b40c920c3ea650dac2d0b07c6
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2023-01-06 18:48:00 +0530

    feat(models): Add base ORM class

---
+++ models/orm/base.py
@@ -0,0 +1,50 @@
+"""
+Base SQLAlchemy model with common fields and configurations.
+"""
+
+from datetime import datetime
+from typing import Any
+from sqlalchemy import DateTime, func, MetaData
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+# Naming convention for constraints
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+
+@as_declarative(metadata=metadata)
+class Base:
+    """Base class for all SQLAlchemy models."""
+    
+    id: Any
+    __name__: str
+    
+    # Generate __tablename__ automatically
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+
+class TimestampMixin:
+    """Mixin to add created_at and updated_at timestamps."""
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False
+    ) 
