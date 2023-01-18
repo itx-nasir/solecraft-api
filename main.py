b41aef8f331c87f02ea37ff623301f2edd8d1162
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2023-01-18 12:44:00 +0530

    feat(api): Setup FastAPI application and wire up routers

---
+++ main.py
@@ -0,0 +1,176 @@
+"""
+Main FastAPI application entry point.
+"""
+
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import structlog
+import sentry_sdk
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from datetime import datetime
+
+from core.config import settings
+from infrastructure.database import init_database, close_database
+from models.schemas import HealthCheck, ErrorResponse
+
+
+# Configure structured logging
+structlog.configure(
+    processors=[
+        structlog.stdlib.filter_by_level,
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.UnicodeDecoder(),
+        structlog.processors.JSONRenderer()
+    ],
+    context_class=dict,
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
+
+logger = structlog.get_logger(__name__)
+
+
+# Initialize Sentry for error tracking
+if settings.sentry_dsn:
+    sentry_sdk.init(
+        dsn=settings.sentry_dsn,
+        integrations=[FastApiIntegration(auto_enabling=True)],
+        traces_sample_rate=1.0 if settings.debug else 0.1,
+        environment=settings.environment,
+    )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Application lifespan context manager."""
+    # Startup
+    logger.info("Starting up SoleCraft API", version=settings.app_version)
+    try:
+        await init_database()
+        logger.info("Application startup completed")
+        yield
+    except Exception as e:
+        logger.error("Failed to start application", error=str(e))
+        raise
+    finally:
+        # Shutdown
+        logger.info("Shutting down SoleCraft API")
+        try:
+            await close_database()
+            logger.info("Application shutdown completed")
+        except Exception as e:
+            logger.error("Error during shutdown", error=str(e))
+
+
+# Create FastAPI application
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+    description="Production-ready backend API for customizable shoes selling platform",
+    docs_url="/docs" if settings.debug else None,
+    redoc_url="/redoc" if settings.debug else None,
+    openapi_url="/openapi.json" if settings.debug else None,
+    lifespan=lifespan
+)
+
+# Import and include API routers
+from api.auth import router as auth_router
+from api.users import router as users_router
+
+app.include_router(auth_router)
+app.include_router(users_router)
+
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Global exception handler
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc: HTTPException):
+    """Handle HTTP exceptions."""
+    logger.warning(
+        "HTTP exception occurred",
+        status_code=exc.status_code,
+        detail=exc.detail,
+        path=request.url.path
+    )
+    
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=ErrorResponse(
+            message=exc.detail,
+            error_code=f"HTTP_{exc.status_code}"
+        ).model_dump()
+    )
+
+
+@app.exception_handler(Exception)
+async def general_exception_handler(request, exc: Exception):
+    """Handle general exceptions."""
+    logger.error(
+        "Unhandled exception occurred",
+        error=str(exc),
+        path=request.url.path,
+        exc_info=True
+    )
+    
+    return JSONResponse(
+        status_code=500,
+        content=ErrorResponse(
+            message="Internal server error",
+            error_code="INTERNAL_ERROR"
+        ).model_dump()
+    )
+
+
+# Health check endpoint
+@app.get("/health", response_model=HealthCheck, tags=["Health"])
+async def health_check():
+    """Health check endpoint."""
+    return HealthCheck(
+        status="healthy",
+        timestamp=datetime.utcnow(),
+        version=settings.app_version,
+        database="connected",
+        redis="connected"
+    )
+
+
+# Root endpoint
+@app.get("/", tags=["Root"])
+async def root():
+    """Root endpoint."""
+    return {
+        "message": f"Welcome to {settings.app_name}",
+        "version": settings.app_version,
+        "docs": "/docs" if settings.debug else "Documentation disabled in production",
+        "health": "/health"
+    }
+
+
+if __name__ == "__main__":
+    import uvicorn
+    
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=settings.debug,
+        log_level="info",
+        access_log=True,
+    ) 
