dc0cdef550437f2708c0cd8cb21319559ac43c16
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2023-01-25 09:45:00 +0530

    fix(auth): Correct authentication middleware logic

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,21 +1,23 @@
 """
-JWT Authentication middleware.
+JWT Authentication middleware with RBAC.
 """
 
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import Optional, List
 from fastapi import HTTPException, status, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 import structlog
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 
 from core.config import settings
 from infrastructure.database import get_async_session
 from repositories.user_repository import UserRepository
 from models.schemas import TokenData
-from models.orm import User
+from models.orm import User, Role
 
 logger = structlog.get_logger(__name__)
 
@@ -78,11 +80,19 @@ async def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(security),
     session: AsyncSession = Depends(get_async_session)
 ) -> User:
-    """Get current authenticated user."""
+    """Get current authenticated user with roles and permissions."""
     token_data = verify_token(credentials.credentials)
     
-    user_repo = UserRepository(session)
-    user = await user_repo.get_by_id(token_data.user_id)
+    # Load user with roles and permissions
+    stmt = (
+        select(User)
+        .options(
+            selectinload(User.roles).selectinload(Role.permissions)
+        )
+        .where(User.id == token_data.user_id)
+    )
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
     
     if user is None:
         raise HTTPException(
@@ -109,7 +119,7 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     return current_user
 
 
-async def get_current_user_optional(
+async def get_optional_current_user(
     credentials: Optional[HTTPAuthorizationCredentials] = Depends(HTTPBearer(auto_error=False)),
     session: AsyncSession = Depends(get_async_session)
 ) -> Optional[User]:
@@ -119,12 +129,169 @@ async def get_current_user_optional(
     
     try:
         token_data = verify_token(credentials.credentials)
-        user_repo = UserRepository(session)
-        user = await user_repo.get_by_id(token_data.user_id)
+        
+        stmt = (
+            select(User)
+            .options(
+                selectinload(User.roles).selectinload(Role.permissions)
+            )
+            .where(User.id == token_data.user_id)
+        )
+        result = await session.execute(stmt)
+        user = result.scalar_one_or_none()
         
         if user and user.is_active:
             return user
     except HTTPException:
         pass
     
-    return None 
+    return None
+
+
+def require_permissions(*required_permissions: str):
+    """
+    Dependency to check for specific permissions.
+    Can require multiple permissions (user must have ALL of them).
+    """
+    async def permission_checker(current_user: User = Depends(get_current_active_user)) -> User:
+        if not required_permissions:
+            return current_user
+        
+        user_permissions = current_user.get_permissions()
+        
+        missing_permissions = []
+        for permission in required_permissions:
+            if permission not in user_permissions:
+                missing_permissions.append(permission)
+        
+        if missing_permissions:
+            logger.warning(
+                "Permission denied",
+                user_id=current_user.id,
+                required_permissions=required_permissions,
+                missing_permissions=missing_permissions,
+                user_permissions=user_permissions
+            )
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Missing permissions: {', '.join(missing_permissions)}"
+            )
+        
+        logger.debug(
+            "Permission granted",
+            user_id=current_user.id,
+            permissions=required_permissions
+        )
+        return current_user
+    
+    return permission_checker
+
+
+def require_any_permission(*required_permissions: str):
+    """
+    Dependency to check for any of the specified permissions.
+    User must have AT LEAST ONE of the specified permissions.
+    """
+    async def permission_checker(current_user: User = Depends(get_current_active_user)) -> User:
+        if not required_permissions:
+            return current_user
+        
+        user_permissions = current_user.get_permissions()
+        
+        has_any_permission = any(
+            permission in user_permissions 
+            for permission in required_permissions
+        )
+        
+        if not has_any_permission:
+            logger.warning(
+                "Permission denied - no required permissions",
+                user_id=current_user.id,
+                required_permissions=required_permissions,
+                user_permissions=user_permissions
+            )
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Requires one of: {', '.join(required_permissions)}"
+            )
+        
+        logger.debug(
+            "Permission granted",
+            user_id=current_user.id,
+            permissions=required_permissions
+        )
+        return current_user
+    
+    return permission_checker
+
+
+def require_role(*required_roles: str):
+    """
+    Dependency to check for specific roles.
+    User must have AT LEAST ONE of the specified roles.
+    """
+    async def role_checker(current_user: User = Depends(get_current_active_user)) -> User:
+        if not required_roles:
+            return current_user
+        
+        user_roles = [role.name for role in current_user.roles if role.is_active]
+        
+        has_required_role = any(
+            role in user_roles 
+            for role in required_roles
+        )
+        
+        if not has_required_role:
+            logger.warning(
+                "Role access denied",
+                user_id=current_user.id,
+                required_roles=required_roles,
+                user_roles=user_roles
+            )
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Requires one of these roles: {', '.join(required_roles)}"
+            )
+        
+        logger.debug(
+            "Role access granted",
+            user_id=current_user.id,
+            roles=required_roles
+        )
+        return current_user
+    
+    return role_checker
+
+
+def require_admin():
+    """Dependency to check for admin access (admin or super_admin role)."""
+    return require_role("admin", "super_admin")
+
+
+def require_staff():
+    """Dependency to check for staff access (any role)."""
+    async def staff_checker(current_user: User = Depends(get_current_active_user)) -> User:
+        if not current_user.is_staff or not current_user.roles:
+            logger.warning(
+                "Staff access denied",
+                user_id=current_user.id,
+                is_staff=current_user.is_staff,
+                roles_count=len(current_user.roles)
+            )
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Staff access required"
+            )
+        
+        return current_user
+    
+    return staff_checker
+
+
+# Backward compatibility - keeping the old function name but using new system
+def has_permission(permission: str):
+    """
+    Backward compatibility function.
+    Use require_permissions() for new code.
+    """
+    return require_permissions(permission) 
